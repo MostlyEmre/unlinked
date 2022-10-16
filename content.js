2d706e63feac14e86550d63b761a93c3f14baa73
MostlyEmre <6238977+MostlyEmre@users.noreply.github.com>
2022-10-16 16:50:12 -0400

    Init commit

---
+++ content.js
@@ -0,0 +1,8 @@
+const body = document.getElementsByTagName("body")[0];
+const authOutlet = body.getElementsByClassName("authentication-outlet")[0];
+
+if (!authOutlet) {
+  console.log("IT IS NOT LOADEDDD");
+} else {
+  console.log("IT IS LOADEDDD");
+}
